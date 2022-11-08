417114a71cf6a3622f6320b7f1d3ac67d0e4d8ed
jueonpark <jueonpark@postech.ac.kr>
2022-11-08 16:39:29 +0900

    add offline execution result

--- run_training.sh
+++ run_training.sh
@@ -26,66 +26,38 @@ export TRACER_TOOL=/home/jueonpark/cxl-simulator/multi_gpu_simulator/util/tracer
 export POST_PROCESSING=/home/jueonpark/cxl-simulator/multi_gpu_simulator/util/tracer_nvbit/tracer_tool/traces-processing/post-traces-processing
 
 # caution!
-# 1803
-export DYNAMIC_KERNEL_LIMIT_START=1803
+# 1939
+export DYNAMIC_KERNEL_LIMIT_START=999998
 export DYNAMIC_KERNEL_LIMIT_END=999999
 
 # additional runtime environment variables for tensorflow
-# export TF_CPP_MIN_VLOG_LEVEL=1
+export TF_CPP_MIN_VLOG_LEVEL=1
 # export ENABLE_CONSOLE=true
 
-# execution options:
-# $1:
-# - vanila for no
-# - pm for pattern matching
-# - fo for fusion offloading
-# - pm_fo for both pattern matching & fusion offlaoding
-# - ideal for ideal offloading
-# - pm_ideal for both pattern matching & ideal offloading
-# $2: trace generation
-# - keyword "trace" given
-# $3: xla_ndpx_use_offline_result
-# - 0 for using GPU results
-# - 1 for using SIM results
-# - on default(no $3 input), use GPU results
-if [ $1 = "vanila" ]
+# EXECUTION OPTION
+TRACE=${1:-notrace}
+EXEC=${2:-help}
+OVERLAP=${3:-0} # 0:one-by-one, 1:one_ndpx-by-many_gpu, 2:one_gpu-by-many_ndpx
+OFFLINE=${4:-0}
+
+if [ $EXEC = "vanilla" ]
 then
   export XLA_FLAGS="--xla_dump_hlo_as_text  --xla_dump_to=./xla_hlo "
-elif [ $1 = "pm" ]
+elif [ $EXEC = "ndpx" ]
 then
-  export XLA_FLAGS="--xla_dump_hlo_as_text --xla_gpu_use_cudnn_batchnorm=true --xla_gpu_use_ndp_batchnorm=true --xla_gpu_use_ndp_bert_pattern=true --xla_gpu_use_ndp_optimizer=true --xla_dump_to=./xla_hlo "
-elif [ $1 = "fo" ]
-then
-  if [ $# = 3 ]
-  then
-    export XLA_FLAGS="--xla_dump_hlo_as_text --xla_ndpx_use_fusion_offloading=true --xla_ndpx_use_offline_result=$3 --xla_dump_to=./xla_hlo "
-  else
-    export XLA_FLAGS="--xla_dump_hlo_as_text --xla_ndpx_use_fusion_offloading=true --xla_ndpx_use_offline_result=0 --xla_dump_to=./xla_hlo "
-  fi
-elif [ $1 = "pm_fo" ]
-then
-  if [ $# = 3 ]
-  then
-    export XLA_FLAGS="--xla_dump_hlo_as_text --xla_gpu_use_cudnn_batchnorm=true --xla_gpu_use_ndp_batchnorm=true --xla_gpu_use_ndp_bert_pattern=true --xla_gpu_use_ndp_optimizer=true --xla_dump_hlo_as_text --xla_ndpx_use_fusion_offloading=true --xla_ndpx_use_offline_result=$3 --xla_dump_to=./xla_hlo "
-  else
-    export XLA_FLAGS="--xla_dump_hlo_as_text --xla_gpu_use_cudnn_batchnorm=true --xla_gpu_use_ndp_batchnorm=true --xla_gpu_use_ndp_bert_pattern=true --xla_gpu_use_ndp_optimizer=true --xla_dump_hlo_as_text --xla_ndpx_use_fusion_offloading=true --xla_ndpx_use_offline_result=0 --xla_dump_to=./xla_hlo "
-  fi
-elif [ $1 = "ideal" ]
-then
-  export XLA_FLAGS="--xla_dump_hlo_as_text --xla_ndpx_use_fusion_offloading=true --xla_ndpx_use_ideal_offloading --xla_dump_to=./xla_hlo "
-elif [ $1 = "pm_ideal" ]
-then
-  export XLA_FLAGS="--xla_dump_hlo_as_text --xla_gpu_use_cudnn_batchnorm=true --xla_gpu_use_ndp_batchnorm=true --xla_gpu_use_ndp_bert_pattern=true --xla_gpu_use_ndp_optimizer=true --xla_dump_hlo_as_text --xla_ndpx_use_fusion_offloading=true --xla_ndpx_use_ideal_offloading --xla_dump_to=./xla_hlo "
+  export XLA_FLAGS="--xla_dump_hlo_as_text --xla_ndpx_use_op_offloading=true --xla_ndpx_use_overlapping_strategy=$OVERLAP --xla_ndpx_use_offline_result=$OFFLINE  --xla_dump_to=./xla_hlo "
 else
-  echo "flags: vanila, pm, fo, pm_fo, ideal, idea_fo"
-	exit 0
+  echo "trace flags: trace, notrace"
+  echo "execution flags: vanilla, ndpx"
+  echo "overlap flags: 0(one-by-one), 1(one_ndpx-by-many_gpu), 2(one_gpu-by-many_ndpx)"
+  echo "offine result flags: 0(real gpu), 1(simulation)"
+  exit 0
 fi
 
-# whether to get trace or not
-if [ $# -ge 2 ] && [ $2 = "trace" ]
+if [ $TRACE = "trace" ]
 then
-  LD_PRELOAD=$TRACER_TOOL python train.py
+  LD_PRELOAD=$TRACER_TOOL python train.py 1> output 2> error
   $POST_PROCESSING ./traces/kernelslist
 else
-  python train.py
+  python train.py 1> output 2> error
 fi
